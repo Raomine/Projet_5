aa6a29a1bced6337c31ccc349e17134943d38b55
Hicham Zarour <zarour.hicham@gmail.com>
2023-08-28 14:48:30 +0200

    Modification du code javascript afin de le raccourcir

--- assets/script.js
+++ assets/script.js
@@ -18,74 +18,39 @@ const slides = [
   },
 ];
 
-const arrowLeft = document.querySelector(".arrow_left");
-const arrowRight = document.querySelector(".arrow_right");
-const dots = document.querySelector(".dots");
-const img = document.querySelector(".banner-img");
-let slidesImg = img.src.substring(img.src.lastIndexOf("/") + 1, img.src.length);
+let arrowLeft = document.querySelector(".arrow_left");
+let arrowRight = document.querySelector(".arrow_right");
+let dots = document.querySelector(".dots");
+let bannerImage = document.querySelector(".banner-img");
+let bannerText = document.querySelector("#banner p");
+let i = 0;
 
-function addDotSelected(element) {
-  element.classList.add("dot_selected");
+for (let i = 0; i < slides.length; i++) {
+  let dot = document.createElement("div");
+  dot.classList.add("dot");
+  dots.appendChild(dot);
 }
-
-function removeDotSelected(element) {
-  element.classList.remove("dot_selected");
-}
-
-function changeImgnTxt(p, nextSlide) {
-  slidesImg = nextSlide.image;
-  p.innerHTML = nextSlide.tagLine;
-}
+let dotSelected = document.querySelectorAll(".dot");
+dotSelected[i].classList.add("dot_selected");
 
 arrowLeft.addEventListener("click", () => {
   console.log("Clic sur la flèche de gauche");
-  let imgNext = 0;
-  let p = document.querySelector("#banner > p");
-  let dot = document.getElementsByClassName("dot");
-
-  for (let i = 0; i < slides.length; i++) {
-    if (slidesImg === slides[0].image) {
-      img.src = `./assets/images/slideshow/${slides[slides.length - 1].image}`;
-      imgNext = slides.length - 1;
-      addDotSelected(dot[imgNext]);
-      removeDotSelected(dot[0]);
-    } else if (slidesImg === slides[i].image) {
-      img.src = `./assets/images/slideshow/${slides[i - 1].image}`;
-      addDotSelected(dot[i - 1]);
-      removeDotSelected(dot[i]);
-      imgNext = i - 1;
-    }
+  dotSelected[i].classList.remove("dot_selected");
+  i = i - 1;
+  if (i < 0) {
+    i = slides.length - 1;
   }
-  changeImgnTxt(p, slides[imgNext]);
+  bannerImage.src = "./assets/images/slideshow/" + slides[i].image;
+  bannerText.innerHTML = slides[i].tagLine;
+  dotSelected[i].classList.add("dot_selected");
 });
 
 arrowRight.addEventListener("click", () => {
   console.log("Clic sur la flèche de droite");
-  let imgNext = 0;
-  let p = document.querySelector("#banner > p");
-  let dot = document.getElementsByClassName("dot");
+  dotSelected[i].classList.remove("dot_selected");
+  i = (i + 1) % slides.length;
 
-  for (let i = 0; i < slides.length; i++) {
-    if (slidesImg === slides[slides.length - 1].image) {
-      img.src = `./assets/images/slideshow/${slides[0].image}`;
-      addDotSelected(dot[0]);
-      removeDotSelected(dot[slides.length - 1]);
-    } else if (slidesImg === slides[i].image) {
-      img.src = `./assets/images/slideshow/${slides[i + 1].image}`;
-      addDotSelected(dot[i + 1]);
-      removeDotSelected(dot[i]);
-      imgNext = i + 1;
-    }
-  }
-  changeImgnTxt(p, slides[imgNext]);
+  bannerImage.src = "./assets/images/slideshow/" + slides[i].image;
+  bannerText.innerHTML = slides[i].tagLine;
+  dotSelected[i].classList.add("dot_selected");
 });
-
-for (i = 0; i < slides.length; i++) {
-  let dot = document.createElement("div");
-  dot.classList.add("dot");
-  dots.appendChild(dot);
-
-  if (slidesImg === slides[i].image) {
-    dot.classList.add("dot_selected");
-  }
-}
